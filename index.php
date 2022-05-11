d4aea2c47470db7d2d59dc844b5c01ce146c55e6
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-11 16:05:19 +0200

    creazione e stampa di istanze padre e figlio

--- index.php
+++ index.php
@@ -25,9 +25,46 @@ $pateC = new Food ( "Patè di salmone", 6, "GIU", "Salmone" );
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>E-commerce</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row row-cols-4 py-5">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $toy1 -> name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $toy1 -> price?>,00€</h6>
+                        <p class="card-text"><?php echo $toy1 -> id ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $crocchetteA -> name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $crocchetteA -> price ?>,00€</h6>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $crocchetteA -> scadenza ?></h6>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $crocchetteA -> protein ?></h6>
+                        <p class="card-text"><?php echo $crocchetteA -> id ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $pateC -> name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $pateC -> price ?>,00€</h6>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $pateC -> scadenza ?></h6>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $pateC -> protein ?></h6>
+                        <p class="card-text"><?php echo $pateC -> id ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
